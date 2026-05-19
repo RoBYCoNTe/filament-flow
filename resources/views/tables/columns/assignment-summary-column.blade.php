39fed5f4c8bce06fac4cb74c07e61c84a0546321
Roberto Conte Rosito <roberto.conterosito@applica.guru>
2026-05-19 15:18:51 +0200

    feat: three-tier opacity by assignment type (primary/secondary/viewer)

--- resources/views/tables/columns/assignment-summary-column.blade.php
+++ resources/views/tables/columns/assignment-summary-column.blade.php
@@ -28,7 +28,11 @@
                 @php
                     $colorIndex = crc32($user['name']) % count($colors);
                     $ringColor = $typeRingColors[$user['assignment_type']] ?? 'ring-white dark:ring-gray-900';
-                    $opacityClass = $user['assignment_type'] === 'secondary' ? 'opacity-75' : '';
+                    $opacityClass = match($user['assignment_type']) {
+                        'secondary' => 'opacity-75',
+                        'viewer'    => 'opacity-50',
+                        default     => '',
+                    };
                     $decorator = $avatarDecorator ? $avatarDecorator($user) : null;
                 @endphp
                 <div class="relative flex h-8 w-8 items-center justify-center rounded-full ring-2 {{ $ringColor }} {{ $colors[$colorIndex] }} {{ $opacityClass }} text-xs font-semibold text-white"
